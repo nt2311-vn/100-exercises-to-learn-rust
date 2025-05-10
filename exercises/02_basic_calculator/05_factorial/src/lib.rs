073b733f1d0e75aad7d20d4a12b8db21cdc38775
nt2311-vn <nguyentoan231196@gmail.com>
2025-05-10 21:17:42 +0700

    complete the factorial function in rust

--- exercises/02_basic_calculator/05_factorial/src/lib.rs
+++ exercises/02_basic_calculator/05_factorial/src/lib.rs
@@ -10,6 +10,13 @@
 //
 // Use only what you learned! No loops yet, so you'll have to use recursion!
 
+fn factorial(number: u32) -> u32 {
+    match number {
+        0 => 1,
+        n => n * factorial(n - 1),
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use crate::factorial;
